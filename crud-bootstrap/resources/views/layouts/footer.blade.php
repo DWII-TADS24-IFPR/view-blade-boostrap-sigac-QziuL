1b37e8418d29eb54c82375e874dee89a99904603
QziuL <lf.quinholi2@gmail.com>
2025-05-12 11:20:40 -0300

    feat: add leayouts and styles

---
+++ crud-bootstrap/resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <footer>
+        <div class="container">
+            <p class="text-center">SIGAC - Sistema de Gerenciamento de Atividades Complementares</p>
+            <p class="text-center">Todos os direitos reservados &copy; {{ date('Y') }}</p>
+        </div>
+    </footer>
+    <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+            <div class="d-flex align-items-center justify-content-between small">
+                <div class="text-muted">Copyright &copy; SIGAC - Sistema de Gerenciamento de Atividades Complementares 2025</div>
+                <div>
+                    <a href="#">Privacy Policy</a>
+                    &middot;
+                    <a href="#">Terms &amp; Conditions</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
