7be90194f40b6463058f81cbd5ca758e6d9e08db
QziuL <lf.quinholi2@gmail.com>
2025-05-13 10:50:47 -0300

    feat: methods crud

--- crud-bootstrap/app/Http/Controllers/AlunoController.php
+++ crud-bootstrap/app/Http/Controllers/AlunoController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Aluno;
+use App\Repositories\AlunoRepository;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AlunoController extends Controller
 {
-    private $regrasValidacao = [
+    private AlunoRepository $repository;
+    private array $regrasValidacao = [
         'nome'      => 'required|min:4|max:255',
         'email'     => 'required|email|max:255',
         'cpf'       => 'required|min:11|max:11',
@@ -17,60 +21,90 @@ class AlunoController extends Controller
         'curso_id'  => 'required',
     ];
 
-    private $mensagemErro = [
+    private array $mensagemErro = [
         'nome.required' => 'O campo nome é obrigatório.',
         'email.required' => 'O campo email é obrigatório.',
         'cpf.required' => 'O campo cpf é obrigatório.',
         'senha.required' => 'O campo senha é obrigatório',
     ];
 
-    public function index()
+    public function index(): View
     {
-        return view('aluno.aluno');
+        $alunos = $this->repository->selectAll();
+        return view('aluno.aluno')->with('alunos', $alunos);
     }
 
-    public function create()
+    public function create(): View
     {
         return view('aluno.create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        $request->validate($this->regrasValidacao,
-                          $this->mensagemErro
-        );
+        $request->validate($this->regrasValidacao, $this->mensagemErro);
 
+//        $aluno = new Aluno(
+//            $request->nome,
+//            $request->email,
+//            $request->cpf,
+//            $request->senha,
+//            $request->user_id,
+//            $request->turma_id,
+//            $request->curso_id
+//        );
 
-        $aluno = new Aluno(
-            $request->nome,
-            $request->email,
-            $request->cpf,
-            $request->senha,
-            $request->user_id,
-            $request->turma_id,
-            $request->curso_id
-        );
+        $aluno = new Aluno();
+        $aluno->nome = $request->get('nome');
+        $aluno->email = $request->get('email');
+        $aluno->cpf = $request->get('cpf');
+        $aluno->senha = $request->get('senha');
+        $aluno->user_id = $request->get('user_id');
+        $aluno->turma_id = $request->get('turma_id');
+        $aluno->curso_id = $request->get('curso_id');
 
-        $aluno->save();
+        $this->repository->save($aluno);
+        return redirect()->route('aluno.index')->with('success', 'Aluno cadastrado com sucesso!');
     }
 
-    public function show(string $id)
+    public function show(string $id): View | RedirectResponse
     {
-        //
+        $aluno = $this->repository->findById($id);
+
+        return ($aluno)
+            ?  view('aluno.show')->with('aluno', $aluno)
+            :  redirect()->route('aluno.index')->with('error', 'Aluno inexistente.');
     }
 
-    public function edit(string $id)
+    public function edit(string $id): View | RedirectResponse
     {
-        //
+        $aluno = $this->repository->findById($id);
+
+        return ($aluno)
+            ? view('aluno.edit')->with('aluno', $aluno)
+            : redirect()->route('aluno.index')->with('error', 'Aluno inexistente.');
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): RedirectResponse
     {
-        //
+        $aluno = $this->repository->findById($id);
+
+        if (!$aluno)
+            return redirect()->route('aluno.index')->with('error', 'Aluno inexistente.');
+
+        $request->validate($this->regrasValidacao, $this->mensagemErro);
+        $aluno->update($request->all());
+
+        return redirect()->route('aluno.index')->with('success', 'Aluno atualizado com sucesso!');
     }
 
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
-        //
+        $aluno = $this->repository->findById($id);
+
+        if (!$aluno)
+            return redirect()->route('aluno.index')->with('error', 'Aluno inexistente.');
+
+        $aluno->softDelete();
+        return redirect()->route('aluno.index')->with('success', 'Aluno removido com sucesso!');
     }
 }
