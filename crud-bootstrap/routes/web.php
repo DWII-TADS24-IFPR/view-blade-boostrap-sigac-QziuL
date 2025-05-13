d3d9669973754b3b40ea4a089b7d172a78ecec1b
QziuL <lf.quinholi2@gmail.com>
2025-05-12 23:54:04 -0300

    feat: add routes for Aluno

--- crud-bootstrap/routes/web.php
+++ crud-bootstrap/routes/web.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('layouts.app');
 })->name('home');
 
 // Rotas para Aluno
-Route::get('/aluno', function() {
-    return view('aluno.aluno');
-});
+Route::get('/aluno', [AlunoController::class, 'index'])->name('aluno.index');
+Route::get('/aluno/create', [AlunoController::class, 'create'])->name('aluno.create');
+Route::post('/aluno/create', [AlunoController::class, 'store'])->name('aluno.store');
 
 // Rotas para Eixo
 Route::prefix('/eixo')->group(function(){
