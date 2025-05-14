6c6672e401b9d2c9d474a340e02cb8e53bb9a89a
QziuL <lf.quinholi2@gmail.com>
2025-05-13 22:05:38 -0300

    feat: add CRUD for Eixo

--- crud-bootstrap/app/Http/Controllers/EixoController.php
+++ crud-bootstrap/app/Http/Controllers/EixoController.php
@@ -4,78 +4,86 @@ namespace App\Http\Controllers;
 
 use App\Models\Eixo;
 use App\Repositories\EixoRepository;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class EixoController extends Controller
 {
-    protected EixoRepository $repository;
+    private EixoRepository $repository;
 
-    public function __construct(){
-        $this->repository = new EixoRepository();
-    }
+    private array $regrasValidacao = [
+        'nome' => 'required|min:3|max:100',
+    ];
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): object
+    private array $mensagemErro = [
+        'nome.required' => 'O campo nome é obrigatório.',
+        'nome.min' => 'O campo nome deve ter mais de 3 caracteres.',
+        'nome.max' => 'O campo nome deve ter menos de 100 caracteres.',
+    ];
+
+    public function __construct() { $this->repository = new EixoRepository(); }
+
+    public function index(): View | RedirectResponse
     {
-        return $this->repository->selectAll();
+        // Se não tiver dados registrados, exibir na View dados nulos
+        $eixos = $this->repository->selectAll();
+        return view('eixo.index', compact('eixos'));
+
+//        return ($eixos)
+//            ? view('eixo.index', compact('eixos'))
+//            : redirect()->route("home")->with("error", "Nenhum Eixo registrado.");
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('eixo.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): string
+    public function store(Request $request): RedirectResponse
     {
-        $obj = new Eixo();
-        $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
-        $this->repository->save($obj);
-        return "<h1>Store - OK!</h1>";
+        $request->validate($this->regrasValidacao, $this->mensagemErro);
+
+        $eixo = new Eixo();
+        $eixo->setNome(mb_strtoupper($request->get('nome'), 'UTF-8'));
+        $this->repository->save($eixo);
+
+        return redirect()->route('eixo.index')->with('success', 'Eixo cadastrado com sucesso!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id): object {
-        return $this->repository->findById($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function show(string $id): View | RedirectResponse
     {
-        //
+        $eixo = $this->repository->findById($id);
+        return ($eixo)
+            ? redirect()->view('eixo.show', compact('eixo'))
+            : redirect()->route('eixo.index')->with('error', 'Eixo não encontrado.');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id): string {
-        $obj = $this->repository->findById($id);
-        if(isset($obj)) {
-            $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
-            $this->repository->save($obj);
-            return "<h1>Update - OK!</h1>";
+    public function edit(string $id): View | RedirectResponse
+    {
+        $eixo = $this->repository->findById($id);
+        return ($eixo)
+            ? redirect()->view('eixo.edit', compact('eixo'))
+            : redirect()->route('eixo.index')->with('error', 'Eixo não encontrado.');
+    }
+
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $eixo = $this->repository->findById($id);
+
+        if(isset($eixo)) {
+            $eixo->setNome(mb_strtoupper($request->get('nome'), 'UTF-8'));
+            $this->repository->save($eixo);
+            return redirect()->route('eixo.index')->with('success', 'Eixo atualizado com sucesso!');
         }
-        return "<h1>Update - Not found Eixo!</h1>";
+
+        return redirect()->route('eixo.index')->with('error', 'Eixo não encontrado.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id): string
+    public function destroy(string $id): RedirectResponse
     {
-        if($this->repository->delete($id))
-            return "<h1>Delete - OK!</h1>";
-        return "<h1>Delete - Not found Eixo!</h1>";
+        return ($this->repository->delete($id))
+            ? redirect()->route('eixo.index')->with('sucess', 'Eixo deletado com sucesso!')
+            : redirect()->route('eixo.index')->with('error', 'Falha ao deletar.');
     }
 }
