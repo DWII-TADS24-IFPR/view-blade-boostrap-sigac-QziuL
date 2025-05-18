b9865655c130ea67eff25a0828151d122d4ae056
QziuL <lf.quinholi2@gmail.com>
2025-05-18 19:07:44 -0300

    feat: add SoftDeletes + gets and sets methods for private attributes

--- crud-bootstrap/app/Models/Turma.php
+++ crud-bootstrap/app/Models/Turma.php
@@ -5,12 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Turma extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'turmas';
 
     protected $fillable = ['curso_id', 'ano'];
+
+    private int $ano;
+    private int $curso_id;
+
+    public function getAno(): int
+    {
+        return $this->ano;
+    }
+
+    public function setAno(int $ano): void
+    {
+        $this->ano = $ano;
+    }
+
+    public function getCursoId(): int
+    {
+        return $this->curso_id;
+    }
+
+    public function setCursoId(int $curso_id): void
+    {
+        $this->curso_id = $curso_id;
+    }
 
     public function curso(): BelongsTo{
         return $this->belongsTo(Curso::class);
