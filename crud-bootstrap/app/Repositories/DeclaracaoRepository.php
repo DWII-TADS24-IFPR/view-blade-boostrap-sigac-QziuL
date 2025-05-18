c0caf17f5c2613e0412f5a4ac4fcb87daaa56730
QziuL <lf.quinholi2@gmail.com>
2025-05-18 17:30:08 -0300

    feat: add repository for Declaracao

---
+++ crud-bootstrap/app/Repositories/DeclaracaoRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use App\Models\Declaracao;
+use App\Repositories\Repository;
+
+class DeclaracaoRepository extends Repository
+{
+    public function __construct()
+    {
+        parent::__construct(new Declaracao());
+    }
+}
