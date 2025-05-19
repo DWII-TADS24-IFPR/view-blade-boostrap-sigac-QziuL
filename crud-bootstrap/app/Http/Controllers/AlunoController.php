cbdc024419973d92629db4d7a1ce92718674b2d3
QziuL <lf.quinholi2@gmail.com>
2025-05-18 23:00:33 -0300

    feat: add repositories for Curso + Turma and improved logic for   store aluno method (not finished)

--- crud-bootstrap/app/Http/Controllers/AlunoController.php
+++ crud-bootstrap/app/Http/Controllers/AlunoController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Aluno;
+use App\Models\User;
 use App\Repositories\AlunoRepository;
+use App\Repositories\CursoRepository;
+use App\Repositories\TurmaRepository;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use function PHPUnit\Framework\isEmpty;
 
 class AlunoController extends Controller
 {
     private AlunoRepository $repository;
+    private CursoRepository $cursoRepository;
+    private TurmaRepository $turmaRepository;
+    private UserController $userController;
 
     private array $regrasValidacao = [
         'nome'      => 'required|min:4|max:255',
@@ -32,6 +39,8 @@ class AlunoController extends Controller
     public function __construct()
     {
         $this->repository = new AlunoRepository();
+        $this->cursoRepository = new CursoRepository();
+        $this->turmaRepository = new TurmaRepository();
     }
 
     public function index(): View
@@ -43,40 +52,45 @@ class AlunoController extends Controller
 
     public function create(): View
     {
-        return view('aluno.create');
+        $cursos = $this->cursoRepository->selectAll();
+        $turmas = $this->turmaRepository->selectAll();
+        return view('aluno.create', compact('cursos', 'turmas'));
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request): View
     {
         $request->validate($this->regrasValidacao, $this->mensagemErro);
+
+        $this->userController->store($request);
+        $user = $this->userController->findByEmail($request->get('email'));
 
         $aluno = new Aluno();
         $aluno->setNome(mb_strtoupper($request->get('nome'), 'UTF-8'));
         $aluno->setEmail($request->get('email'));
         $aluno->setCpf($request->get('cpf'));
-        $aluno->setSenha($request->get('senha'));
-        $aluno->setUserId($request->get('user_id'));
+        $aluno->setSenha(bcrypt($request->get('senha')));
+        $aluno->setUserId($user->id);
         $aluno->setTurmaId($request->get('turma_id'));
         $aluno->setCursoId($request->get('curso_id'));
-
         $this->repository->save($aluno);
-        return redirect()->route('aluno.index')->with('success', 'Aluno cadastrado com sucesso!');
+
+        return view('aluno.index')->with('success', 'Aluno cadastrado com sucesso!');
     }
 
-    public function show(string $id): View | RedirectResponse
+    public function show(string $id): View
     {
         $aluno = $this->find($id);
         return ($aluno)
             ?  view('aluno.show')->with('aluno', $aluno)
-            :  redirect()->route('aluno.index')->with('error', 'Aluno inexistente.');
+            :  view('aluno.index')->with('error', 'Aluno inexistente.');
     }
 
-    public function edit(string $id): View | RedirectResponse
+    public function edit(string $id): View
     {
         $aluno = $this->find($id);
         return ($aluno)
             ? view('aluno.edit')->with('aluno', $aluno)
-            : redirect()->route('aluno.index')->with('error', 'Aluno inexistente.');
+            : view('aluno.index')->with('error', 'Aluno inexistente.');
     }
 
     public function update(Request $request, string $id): View
@@ -91,11 +105,11 @@ class AlunoController extends Controller
         return view('aluno.edit')->with('error', 'Aluno inexistente.');
     }
 
-    public function destroy(string $id): RedirectResponse
+    public function destroy(string $id): View
     {
         return ($this->find($id)->softDeletes())
-            ? redirect()->route('aluno.index')->with('success', 'Aluno removido com sucesso!')
-            : redirect()->route('aluno.index')->with('error', 'Falha ao deletar.');
+            ? view('aluno.index')->with('success', 'Aluno removido com sucesso!')
+            : view('aluno.index')->with('error', 'Falha ao deletar.');
     }
 
     private function find(int $id): object | null
